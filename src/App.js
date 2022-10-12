3ce11f23608ce5801037b805b2ae1289301e47f2
Samurai2303 <meleschenko.andriy@gmail.com>
2022-10-12 14:05:40 +0300

    typescript lesson 1

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import './App.css';
 
 function App() {
 
-  return (
-      <div>
-
-      </div>
-  );
+    return (
+        <div>
+            App
+        </div>
+    );
 }
 
 export default App;
